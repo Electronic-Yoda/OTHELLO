80d335c758e273d05be7ff15425bd60297442c59
Raymond <raymond.yang121@outlook.com>
2021-08-05 00:38:04 -0400

    completed (untested) gameLogic functions which will return a dictionary containing disk placement information

--- src/game_logic.py
+++ src/game_logic.py
@@ -14,6 +14,7 @@ class GameLogic:
         self.thisTurnColor = None
     
     def boardSetUp(self):
+        self.thisTurnColor = 'Black'
         for i in range(boardSize):
             for j in range(boardSize):
                 self.board[i][j].color = "U"
@@ -26,26 +27,75 @@ class GameLogic:
     def getPlacementInfo(self, i, j, color) -> dict:
         board = self.board
         info = dict({'legal': None, 'tileList': None, 'color': None})
+
+        color = self.thisTurnColor
+        info['color'] = color
+        info['tileList'] = []
         
         if board[i][j].color != "U":
             info['legal'] = False
             return info
         
-        # Loop through each direction
+        # Loop through each direction and get each direction's info
         for delta_i in range(-1,2):
             for delta_j in range(-1, 2):
                 if delta_i != 0 and delta_j != 0:
-                    pass
-                
+                    # Call directionInfo
+                    dirInfo = self.directionInfo(i, j, delta_i, delta_j, color)
+                    if dirInfo['legal'] == True:
+                        info['legal'] = True
+                        # Load tileList
+                        tileList = dirInfo['tileList']
+                        info['tileList'].append(tileList)
+        return info
 
+
+                        
     
     def directionInfo(self, i, j, delta_i, delta_j, color):
-        
-        for i in range(boardSize):
-            pass
+        board = self.board
+        dirInfo = dict({'legal': None, 'tileList': None})
 
+        # Check if first color beside is an opposite color
+        cur_i = i + delta_i
+        cur_j = j + delta_j
+        if not self.inBounds(cur_i, cur_j):
+            dirInfo['legal'] = False
+            return dirInfo
+        if board[cur_i][cur_j].color != self.oppColor(color):
+            dirInfo['legal'] = False
+            return dirInfo
+        
+        # if opposite color, check the rest of the line
+        tileList = []
+        for a in range(boardSize):
+            if not self.inBounds(cur_i, cur_j):
+                dirInfo['legal'] = False
+                return dirInfo
+            if board[cur_i][cur_j].color == 'U':
+                dirInfo['legal'] = False
+                return dirInfo
+
+            tileList.append((cur_i, cur_j))
+            if board[cur_i][cur_j].color == color:
+                dirInfo['legal'] = True
+                dirInfo['tileList'] = tileList
+                return dirInfo
+            cur_i += delta_i  
+            cur_j += delta_j
+            
+
+                
     def inBounds(self, i, j) -> bool:
         if 0 <= i and i < boardSize and 0 <= j and j < boardSize:
             return True
         else:
             return False
+
+    def oppColor(self, color):
+        if color == 'B':
+            return 'W'
+        elif color == 'W':
+            return 'B'
+        else:
+            print("oppColor Error")
